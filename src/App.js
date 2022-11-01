474e10b8f1fb41db8e62697933ebb1e437fbb6fe
victoryeah <victor@yeahdevelop.com>
2022-11-01 19:54:26 +0100

    Inputs, and console output of final position

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Box, FormControl, TextField, Grid, Button } from '@mui/material'
+
+
+function finalPosition(move) {
+  let l = move.length;
+  let countUp = 0, countDown = 0;
+  let countLeft = 0, countRight = 0;
+
+  for (let i = 0; i < l; i++)
+  {
+      if (move[i] === 'F')
+          countUp++;
+
+      else if (move[i] === 'B')
+          countDown++;
+
+      else if (move[i] === 'L')
+          countLeft++;
+
+      else if (move[i] === 'R')
+          countRight++;
+  }
+
+  console.log("Final Position: ("
+                    + (countRight - countLeft) + ", "
+                    + (countUp - countDown) + ")");
+}
 
 function App() {
+  const [roomWide, setRoomWide] = useState("");
+  const [roomLong, setRoomLong] = useState("");
+  const [robotX, setRobotX] = useState("");
+  const [robotY, setRobotY] = useState("");
+  const [robotDir, setRobotDir] = useState("");
+  const [robotInstructions, setRobotInstructions] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container padding={8} spacing={2} className="App">
+      <Grid item xs={12}>
+        <FormControl>
+          <TextField 
+            label="How wide is the room?" 
+            onChange={event => setRoomWide(event.target.value)}
+          />
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl>
+          <TextField label="How long is the room?" 
+          onChange={event => setRoomLong(event.target.value)}/>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl>
+          <TextField label="Robot start position X" 
+          onChange={event => setRobotX(event.target.value)}/>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl>
+          <TextField label="Robot start position Y" 
+          onChange={event => setRobotY(event.target.value)}/>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl>
+          <TextField label="Where is the robot facing? (N, S, E, W)"
+            onChange={event => setRobotDir(event.target.value)}/>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <FormControl>
+          <TextField label="How should the robot walk? (FRLB...)"
+            onChange={event => setRobotInstructions(event.target.value)}/>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" onClick={() => finalPosition(robotInstructions)}>Set robot instructions</Button>
+      </Grid>
+    </Grid>
   );
+  
 }
 
 export default App;
